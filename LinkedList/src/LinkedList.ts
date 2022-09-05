5b7209b24b4789ec7fcf701626eec34a847c6948
ShMirnajafi <99201911+ShMirnajafi@users.noreply.github.com>
2022-09-05 23:02:48 +0430

    feat: iterator added

--- LinkedList/src/LinkedList.ts
+++ LinkedList/src/LinkedList.ts
@@ -1,5 +1,5 @@
 interface ILinkedList<T> {
-    addEnd: (value: T) => void;
+    add: (value: T) => void;
 
     addFirst: (value: T) => void;
 
@@ -8,41 +8,128 @@ interface ILinkedList<T> {
     print: () => void;
 }
 
-type node<T> = [
-    data: T | null,
-    next_node: node<T> | null
-];
+class node<T> {
+    private data: T;
+    private next_node: node<T> | null;
+    private previous_node: node<T> | null;
 
-class LinkedList<T> implements ILinkedList<T> {
-    private head: node<T> = [null, null];
-
-    constructor (value: T) {
-        this.head = [value, null];
+    constructor (data: T) {
+        this.data = data;
+        this.next_node = null;
+        this.previous_node = null;
+    }
+    
+    public get nextNode(): node<T> | null {
+        return this.next_node;
     }
 
-    public addEnd(value: T): void {
+    public get value(): T | null {
+        return this.data;
+    }
+
+    public get previousNode(): node<T> |null {
+        return this.previous_node;
+    }
+
+    
+    public setPrevious(node: node<T>) {
+        this.previous_node = node;
+    }
+    
+    
+    public addNode (node: node<T> | null): void {
+        this.next_node = node;
+    }
+
+    public removeNextNode (): void {
+        this.next_node = null;
+    }
+}
+
+class LinkedList<T> implements ILinkedList<T> {
+    private head: node<T>;
+    private final_node: node<T>;
+
+    [Symbol.iterator]() {
+        let temp_node = this.head;
+        let temp_node2 = this.final_node;
+        let temp_head = this.head;
+        let temp_final_node = this.final_node;
+
+        return {
+            next(): IteratorResult<T> {
+                let temp_return_node = temp_node;
+                if (temp_node.nextNode === null) {
+                    temp_node = temp_head;
+                    return {
+                        done: true,
+                        value: temp_return_node.value
+                    }
+                } else {
+                    temp_node = temp_node.nextNode;
+                    return {
+                        done: false,
+                        value: temp_return_node.value!
+                    }
+                }
+            },
+            
+            previous(): IteratorResult<T> {
+                let temp_return_node = temp_node2;
+                if (temp_node2.previousNode === null) {
+                    temp_node2 = temp_final_node;
+                    return {
+                        done: true,
+                        value: temp_return_node.value
+                    }
+                } else {
+                    temp_node2 = temp_node2.previousNode;
+                    return {
+                        done: false,
+                        value: temp_return_node.value!
+                    }
+                }
+            }
+        };
+    }
+
+    constructor (value: T) {
+        this.head = new node(value);
+        this.final_node = new node(value);
+    }
+
+    public add(value: T): void {
         let temp_node: node<T> = this.head;
-        while (temp_node[1] !== null) {
-            temp_node = temp_node[1];
+        while (temp_node.nextNode !== null) {
+            temp_node = temp_node.nextNode;
         }
-        temp_node[1] = [value, null];
-        temp_node[1][1] = [null, null];
+        temp_node.addNode(new node(value));
+        temp_node.nextNode!.setPrevious(temp_node);
+        this.final_node = temp_node.nextNode!;
+        console.log(this.final_node);
     }
 
     public addFirst(value: T): void {
         let temp_node: node<T> = this.head; 
-        let fisrt_node: node<T> = [value, temp_node];
+        let fisrt_node: node<T> = new node(value);
+        fisrt_node.addNode(temp_node);
+        temp_node.setPrevious(fisrt_node);
         this.head = fisrt_node;
     }
 
     public remove(value: T): boolean {
         let temp_node: node<T> = this.head;
-        while (temp_node[1] !== null) {
-            if (temp_node[1][0] === value) {
-                temp_node[1] = temp_node[1][1];
+        while (temp_node.nextNode !== null) {
+            if (temp_node.nextNode.value === value) {
+                let temp_next_node: node<T> | null = temp_node.nextNode.nextNode;
+                temp_node.removeNextNode();
+                if (temp_next_node !== null) {
+                    temp_node.addNode(temp_next_node);
+                    temp_next_node.setPrevious(temp_node);
+                }
                 return true;
             }
-            temp_node = temp_node[1];
+            temp_node = temp_node.nextNode;
         }
         return false;
     }
@@ -50,28 +137,47 @@ class LinkedList<T> implements ILinkedList<T> {
     public print(): void {
         let temp_node: node<T> = this.head;
         console.log('[');
-        while (temp_node[1] !== null) {
-            if (temp_node[0] === null) {
-                temp_node = temp_node[1];
-            } else {
-                console.log(temp_node[0]);
-                temp_node = temp_node[1];   
-            }
+        while (temp_node.nextNode !== null) {
+            console.log(temp_node.value);
+            temp_node = temp_node.nextNode;
         }
         console.log(']');
     }
 }
 
 let linkedlist = new LinkedList<number>(1);
+let iterator = linkedlist[Symbol.iterator]();
 
-linkedlist.addEnd(2);
-linkedlist.addEnd(4);
-linkedlist.addEnd(7);
-linkedlist.addEnd(0);
+linkedlist.add(2);
+linkedlist.add(4);
+linkedlist.add(7);
+linkedlist.add(0);
 linkedlist.print();
+
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log("-------------");
+console.log(iterator.previous());
+console.log(iterator.previous());
+console.log(iterator.previous());
+console.log(iterator.previous());
+
+
+console.log("with iterator:");
+for (const n of linkedlist) {
+    console.log(n);
+}
 
 linkedlist.remove(2);
 linkedlist.print();
+console.log("with iterator:");
+for (const n of linkedlist) {
+    console.log(n);
+}
 
 linkedlist.addFirst(-1);
 linkedlist.print();
@@ -79,5 +185,5 @@ linkedlist.print();
 linkedlist.addFirst(20);
 linkedlist.print();
 
-console.log(linkedlist.remove(-1));
+console.log(linkedlist.remove(-1) + "\n" + linkedlist.remove(-3));
 linkedlist.print();
